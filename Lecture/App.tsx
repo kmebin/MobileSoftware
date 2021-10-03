d8c572fc427289037504360108022f1fd02a4045
kmebin <blowhuiee@gmail.com>
2021-10-04 01:52:27 +0900

    [add] ch03-1 : color 패키지 적용

--- Lecture/App.tsx
+++ Lecture/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {Colors} from 'react-native-paper';
+import Color from 'color';
 
-// style 속성에 설정되는 값은 '객체'임 -> 스타일 객체
-// 바깥쪽 {} : JSX 문법, 안쪽 {} : 객체를 의미
-// style 속성 값에 배열이 오면, 배열 안의 스타일 객체를 결합하여 하나의 스타일 객체로 만듦
-
-// 1. 인라인 스타일로 적용하는 방법 -> 동적인 스타일인 경우 유리
 const App = () => {
   return (
-    <SafeAreaView style={[styles.SafeAreaView, {backgroundColor: 'blue'}]}>
-      <Text style={[styles.text, {color: 'white'}]}>Hello World!</Text>
+    <SafeAreaView style={[styles.SafeAreaView]}>
+      <Text style={[styles.text]}>Hello StyleSheet World!</Text>
     </SafeAreaView>
   );
 };
 
-// 2. StyleSheet API를 사용하는 방법 -> 정적인 스타일인 경우 유리
+// Colors
 const styles = StyleSheet.create({
-  SafeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center'},
-  text: {fontSize: 20},
+  SafeAreaView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.blue500,
+  },
+  text: {
+    fontSize: 20,
+    color: Color(Colors.blue500).alpha(0.7).lighten(0.9).string(),
+  },
 });
 
 export default App;
