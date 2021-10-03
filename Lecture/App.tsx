88b08d0dcf2d1c18b72ab43b098eb3554327daa6
kmebin <blowhuiee@gmail.com>
2021-10-04 01:07:09 +0900

    [add] ch02-4 : text에 onPress 콜백 함수 설정

--- Lecture/App.tsx
+++ Lecture/App.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import {SafeAreaView, Alert, Button} from 'react-native';
-// Touchable 컴포넌트 : 버튼 역할을 할 수 있는 2가지 컴포넌트
-// 단, 한 개의 자식 컴포넌트만 가질 수 있음
-import {TouchableOpacity, TouchableHighlight, Text} from 'react-native';
+// Text 컴포넌트도 onPress 콜백 함수 설정이 가능함
+// 단, 버튼이나 touchable 컴포넌트와 같은 시각 효과는 없음
+import {Text} from 'react-native';
 
 const onPress = () => Alert.alert('pressed!', 'message');
 
-// Text 컴포넌트를 버튼처럼 동작하게 하는 예시
 const App = () => {
   return (
     <SafeAreaView>
       <Button title="Home" onPress={onPress} />
-      <TouchableOpacity onPress={onPress}>
-        <Text>TouchableOpacity</Text>
-      </TouchableOpacity>
-      <TouchableHighlight onPress={onPress}>
-        <Text>TouchableHighlight</Text>
-      </TouchableHighlight>
+      <Text onPress={onPress}>Press Me!</Text>
     </SafeAreaView>
   );
 };
