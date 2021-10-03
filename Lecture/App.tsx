66483f21e33e8a7970a750d2d1e72e4ed0da7577
kmebin <blowhuiee@gmail.com>
2021-10-04 03:23:48 +0900

    [add] ch03-2 : margin, padding, border 적용

--- Lecture/App.tsx
+++ Lecture/App.tsx
@@ -1,35 +1,49 @@
 import React from 'react';
-// Platform API : 실행되는 폰의 os 정보 제공
-// Dimensions API : 현재 실행된 폰의 크기 제공
 import {
   Platform,
   Dimensions,
   StyleSheet,
   SafeAreaView,
   Text,
+  View,
 } from 'react-native';
 import {Colors} from 'react-native-paper';
-import Color from 'color';
+import color from 'color';
 
 const {width, height} = Dimensions.get('window');
 
+// android에서 radius 속성이 있으면 borderColor가 적용되지 않는 현상
+// ios에서 padding 속성이 적용되지 않는 현상
 const App = () => {
   return (
     <SafeAreaView style={[styles.SafeAreaView]}>
       <Text style={[styles.text]}>os : {Platform.OS}</Text>
       <Text style={[styles.text]}>width : {width}</Text>
       <Text style={[styles.text]}>heigth : {height}</Text>
+
+      <View style={[styles.box, styles.border]} />
+      <View style={[styles.box, styles.border, {borderRadius: 20}]} />
+      <View
+        style={[
+          styles.box,
+          styles.border,
+          {borderTopLeftRadius: 30},
+          {borderBottomLeftRadius: 30},
+        ]}
+      />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  // flex 스타일 속성
-  SafeAreaView: {backgroundColor: Colors.blue500, flex: 1}, // height: '100%'와 동일한 값
+  SafeAreaView: {backgroundColor: Colors.blue500, flex: 1, padding: 10},
   text: {
+    marginBottom: 10,
     fontSize: 20,
-    color: Color(Colors.blue500).lighten(0.9).string(),
+    color: color(Colors.blue500).lighten(0.9).string(),
   },
+  box: {height: 100, backgroundColor: Colors.lime500, marginBottom: 10},
+  border: {borderWidth: 10, borderColor: color('black').alpha(0.3).toString()},
 });
 
 export default App;
