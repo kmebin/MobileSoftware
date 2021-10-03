4ddb6248746e17f044db9f0c385a5258fbbc8bc3
kmebin <blowhuiee@gmail.com>
2021-10-04 03:35:00 +0900

    [add] ch03-2 : platform의 select 메서드 사용

--- Lecture/App.tsx
+++ Lecture/App.tsx
@@ -8,42 +8,39 @@ import {
   View,
 } from 'react-native';
 import {Colors} from 'react-native-paper';
-import color from 'color';
 
 const {width, height} = Dimensions.get('window');
 
-// android에서 radius 속성이 있으면 borderColor가 적용되지 않는 현상
-// ios에서 padding 속성이 적용되지 않는 현상
 const App = () => {
   return (
     <SafeAreaView style={[styles.SafeAreaView]}>
-      <Text style={[styles.text]}>os : {Platform.OS}</Text>
-      <Text style={[styles.text]}>width : {width}</Text>
-      <Text style={[styles.text]}>heigth : {height}</Text>
+      <Text style={[styles.text, {color: 'white'}]}>os : {Platform.OS}</Text>
+      <Text style={[styles.text, {color: 'yellow'}]}>width : {width}</Text>
+      <Text style={[styles.text, {color: 'orange'}]}>heigth : {height}</Text>
 
+      <View style={[styles.box, {borderRadius: 10}]} />
       <View style={[styles.box, styles.border]} />
-      <View style={[styles.box, styles.border, {borderRadius: 20}]} />
-      <View
-        style={[
-          styles.box,
-          styles.border,
-          {borderTopLeftRadius: 30},
-          {borderBottomLeftRadius: 30},
-        ]}
-      />
+      <View style={[styles.box, styles.border,{borderRadius: 20}]} />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  SafeAreaView: {backgroundColor: Colors.blue500, flex: 1, padding: 10},
-  text: {
-    marginBottom: 10,
-    fontSize: 20,
-    color: color(Colors.blue500).lighten(0.9).string(),
+  // Platform.select를 사용하여 os마다 다른 값을 적용
+  SafeAreaView: {
+    backgroundColor: Colors.blue500,
+    flex: 1,
+    paddingLeft: Platform.select({ios: 0, android: 20}),
   },
-  box: {height: 100, backgroundColor: Colors.lime500, marginBottom: 10},
-  border: {borderWidth: 10, borderColor: color('black').alpha(0.3).toString()},
+  text: {fontSize: 20, marginVertical: 20, marginLeft: 30},
+  box: {
+    width: '70%',
+    height: 100,
+    backgroundColor: 'white',
+    marginBottom: 10,
+    marginLeft: Platform.select({ios: 20, android: 0}), // ios는 padding 대신 margin을 줌
+  },
+  border: {borderWidth: 10, borderColor: Colors.lime500},
 });
 
 export default App;
