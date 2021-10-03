111a9fc33b2a22728c762d4a5cc4674af9553244
kmebin <blowhuiee@gmail.com>
2021-10-04 01:00:47 +0900

    [add] ch02-4 : touchable 컴포넌트

--- Lecture/App.tsx
+++ Lecture/App.tsx
@@ -1,33 +1,22 @@
 import React from 'react';
 import {SafeAreaView, Alert, Button} from 'react-native';
+// Touchable 컴포넌트 : 버튼 역할을 할 수 있는 2가지 컴포넌트
+// 단, 한 개의 자식 컴포넌트만 가질 수 있음
+import {TouchableOpacity, TouchableHighlight, Text} from 'react-native';
 
-// Button 컴포넌트, Alert API 사용
+const onPress = () => Alert.alert('pressed!', 'message');
+
+// Text 컴포넌트를 버튼처럼 동작하게 하는 예시
 const App = () => {
   return (
     <SafeAreaView>
-      <Button
-        title="Home"
-        onPress={() => Alert.alert('home pressed.', 'message')}
-      />
-      <Button
-        title="Select"
-        onPress={() =>
-          Alert.alert('select pressed.', 'message', [
-            {
-              text: 'No',
-              onPress: () => {
-                console.log('No');
-              },
-            },
-            {
-              text: 'Yes',
-              onPress: () => {
-                console.log('Yes');
-              },
-            },
-          ])
-        }
-      />
+      <Button title="Home" onPress={onPress} />
+      <TouchableOpacity onPress={onPress}>
+        <Text>TouchableOpacity</Text>
+      </TouchableOpacity>
+      <TouchableHighlight onPress={onPress}>
+        <Text>TouchableHighlight</Text>
+      </TouchableHighlight>
     </SafeAreaView>
   );
 };
