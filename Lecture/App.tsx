2265ad87cf5e8bcd5d65fbb7d8c989a0a8d3a756
kmebin <blowhuiee@gmail.com>
2021-10-04 01:13:54 +0900

    [add] ch02-4 : textInput 컴포넌트 사용

--- Lecture/App.tsx
+++ Lecture/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {SafeAreaView, Alert, Button} from 'react-native';
-// Text 컴포넌트도 onPress 콜백 함수 설정이 가능함
-// 단, 버튼이나 touchable 컴포넌트와 같은 시각 효과는 없음
-import {Text} from 'react-native';
-
-const onPress = () => Alert.alert('pressed!', 'message');
+import {SafeAreaView, TextInput} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Button title="Home" onPress={onPress} />
-      <Text onPress={onPress}>Press Me!</Text>
+      <TextInput
+        placeholder="enter your name"
+        onChangeText={(text: string) => console.log(text)}
+        onFocus={() => console.log('onFocus')}
+        onBlur={() => console.log('onBlur')}
+        onEndEditing={() => console.log('onEndEditing')}
+      />
     </SafeAreaView>
   );
 };
