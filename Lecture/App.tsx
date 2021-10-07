bcf4f8d3d30e7a2884d25426191dca3977672267
kmebin <blowhuiee@gmail.com>
2021-10-07 10:58:10 +0900

    [add] ch03-4 : top, content, bottom 적용

--- Lecture/App.tsx
+++ Lecture/App.tsx
@@ -1,60 +1,22 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Platform,
-  SafeAreaView,
-  ImageBackground,
-  Image,
-  View,
-  Text,
-  Alert,
-} from 'react-native';
+import {StyleSheet, SafeAreaView} from 'react-native';
 import {Colors} from 'react-native-paper';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; // 아이콘 컴포넌트 사용하기 위함
-import {createAvatarUrl} from './util/createAvatarUrl';
-
-const avatarUrl = createAvatarUrl();
-const avatarSize = 50;
-const text = 'Almost before we knew it, we had left the ground';
-const onIconPressed = () => Alert.alert('icon pressed');
+import TopBar from './src/screens/TopBar';
+import Content from './src/screens/Content';
+import BottomBar from './src/screens/BottomBar';
 
 const App = () => {
   return (
     <SafeAreaView style={[styles.flex]}>
-      <ImageBackground
-        style={[styles.flex, styles.padding10]}
-        source={require('./assets/images/bg.jpg')}>
-        <Image source={{uri: avatarUrl}} style={[styles.image]} />
-        <View style={[styles.flex, styles.padding10]}>
-          <Text style={[styles.text, styles.regular]}>{text} [regular]</Text>
-          <Text style={[styles.text, styles.medium]}>{text} [medium]</Text>
-          <Text style={[styles.text, styles.semiBold]}>{text} [semi bold]</Text>
-          <Text style={[styles.text, styles.bold]}>{text} [bold]</Text>
-        </View>
-        <Icon
-          name="home"
-          size={50}
-          color={Colors.lightBlue500}
-          onPress={onIconPressed}
-        />
-      </ImageBackground>
+      <TopBar />
+      <Content />
+      <BottomBar />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  flex: {flex: 1},
-  padding10: {padding: 10},
-  image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2},
-  text: {textAlign: 'center', fontSize: 25, color: 'white', marginBottom: 10},
-
-  regular: {fontFamily: 'DancingScript-Regular', fontWeight: '400'},
-  medium: {fontFamily: 'DancingScript-Medium', fontWeight: '500'},
-  semiBold: {fontFamily: 'DancingScript-SemiBold', fontWeight: '600'},
-  bold: {
-    fontFamily: 'DancingScript-Bold',
-    fontWeight: Platform.select({ios: '700', android: '600'}), // android에서는 weight '700'이 적용되지 않음
-  },
+  flex: {flex: 1, backgroundColor: Colors.lightBlue100},
 });
 
 export default App;
