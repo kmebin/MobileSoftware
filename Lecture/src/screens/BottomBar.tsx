21400e3ece651f1891b1e73a6fe1a6b4fb129478
kmebin <blowhuiee@gmail.com>
2021-10-12 15:20:16 +0900

    [add] ch03-4 : justifyContent 사용해서 bottomBar 수정

--- Lecture/src/screens/BottomBar.tsx
+++ Lecture/src/screens/BottomBar.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {Colors} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const title = 'BottomBar';
+const iconSize = 30,
+  iconColor = 'white';
+const icons = [
+  'home',
+  'table-search',
+  'face-profile-woman',
+  'account-settings',
+];
+
 const BottomBar = () => {
-  return (
-    <View style={[styles.view]}>
-      <Text style={[styles.text]}>{title}</Text>
-    </View>
-  );
+  const children = icons.map(name => (
+    <Icon key={name} name={name} size={iconSize} color={iconColor} />
+  ));
+  return <View style={[styles.view]}>{children}</View>;
 };
 
 const styles = StyleSheet.create({
-  view: {padding: 5, backgroundColor: Colors.blue900},
-  text: {fontSize: 20, color: 'white'},
+  view: {
+    flexDirection: 'row',
+    alignItems: 'center', // 자식 컴포넌트의 배치 간격 속성
+    justifyContent: 'space-evenly', // 부모의 여백을 자식의 간격에 반영하는 속성
+    padding: 10,
+    backgroundColor: Colors.lightBlue500,
+  },
 });
 export default BottomBar;
