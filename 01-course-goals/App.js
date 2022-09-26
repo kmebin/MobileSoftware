460ecc4e76c6cccaf14fa6439749d75f4c44a174
kmebin <blowhuiee@gmail.com>
2022-09-26 15:12:33 +0900

    [FIX] babel.config export 오류

---
+++ 01-course-goals/App.js
@@ -0,0 +1,17 @@
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <View>
+        <TextInput placeholder='Your course goals!' />
+        <Button title='Add Goal' />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
