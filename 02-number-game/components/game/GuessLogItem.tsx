d4c8dbfc98e14291f859c8a816155c99d35e4a47
kmebin <blowhuiee@gmail.com>
2022-10-04 13:09:17 +0900

    [ADD] 로그 리스트 아이템 컴포넌트

---
+++ 02-number-game/components/game/GuessLogItem.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from "react-native";
+import colors from "../../constants/colors";
+
+export type GuessLogItemType = {
+  round: number;
+  guess: number;
+};
+
+export default function GuessLogItem({ round, guess }: GuessLogItemType) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>#{round}</Text>
+      <Text style={styles.text}>Opponent's Guess: {guess}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    padding: 12,
+    marginVertical: 8,
+    borderColor: colors.darkPlum,
+    borderWidth: 1,
+    borderRadius: 40,
+    backgroundColor: colors.yellow,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+  },
+  text: {
+    fontFamily: "open-sans",
+  },
+});
